21c318c46244f01d63366c92278432a82169f934
kata8yama <162008868+kata8yama@users.noreply.github.com>
2024-07-28 15:51:23 +0900

    Add files via upload

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -4,12 +4,16 @@
   <meta charset="UTF-8">
   <title>連想配列の課題</title>
 </head>
+
 <body>
+  <p>
 <?php
 $personal_data = ['name' => 'orion', 'price' => 200, 'weight' => '160'];
 
 print_r($personal_data);
 
 ?>
+</p>
 </body>
+
 </html>
