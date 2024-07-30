047ca325a9c214bdd3f213c0ac0dd684c7102b81
kata8yama <162008868+kata8yama@users.noreply.github.com>
2024-07-30 23:11:13 +0900

    Add files via upload

--- kadai_011/foreach.php
+++ kadai_011/foreach.php
@@ -9,7 +9,7 @@
 <p>
 <?php
 // 連想配列
-$variable_names = ['name' => '玉ねぎ', 'price' => 200, 'gender' => '男性', 'address' => '北海道'];
+$variable_name = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
 //  連想配列＄variable_nameのキーと値をコロン（：）で区切り、要素ごとに改行して出力する
         
 foreach ($variable_name as $key => $value) {
