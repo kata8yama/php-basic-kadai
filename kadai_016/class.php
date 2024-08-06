e81c3f96153b21562956b6440c48bd88328bf204
kata8yama <162008868+kata8yama@users.noreply.github.com>
2024-08-06 22:45:42 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  
+  <title>クラスを2つ作ってそれぞれのインスタンスを出力しよう</title>
+<body>
+  <?php
+  class Food {
+    private $name;
+    private $place;
+
+    public function __construct(string $name, int $price) {
+      $this->name = $name;
+      $this->price = $price;
+    }
+
+    public function show_price() {
+      echo $this->price;
+
+    }
+  }
+  class Animal {
+    private $name;
+    private $height;
+    private $weight;
+
+    public function __construct(string $name, int $height, int $weight) {
+      $this->name = $name;
+      $this->height = $height;
+      $this->weight = $weight;
+    }
+
+    public function show_height() {
+      echo $this->height;
+    }
+  }
+
+  $potato = new Food('potato', 250);
+  $dog = new Animal('dog', 60, 5000);
+
+  print_r($potato);
+  echo '<br>';
+  print_r($dog);
+  echo '<br>';
+
+  $potato->show_price();
+  echo '<br>';
+  $dog->show_height();
+
+  ?>
+</body>
+</html>
